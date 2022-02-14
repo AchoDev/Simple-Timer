448a84a3dd6d5c1391463cf5588271ff19ba9ab9
General Shepherd <Hallo017676!>
2022-02-14 21:41:45 +0100

    added minute and millisecond timer and fixed all problems, so it's now usable

--- timer.js
+++ timer.js
@@ -1,26 +1,62 @@
 let time = 0;
-let pause = true;
+let isPaused = true;
 
 const timer = document.getElementById("timer");
 
+let interval;
+let tm;
+
+const curTime = () => {
+    tm = String(time);
+    const arr = (""+tm).split("");
+    
+    const addZero = (num) => {
+        if(num < 10){
+            return "0" + num;
+        } else {
+            return num;
+        }
+    }
+
+    const min = parseFloat(tm) / 100 / 60;
+    let floorMin = Math.floor(min);
+    const decMin = min - floorMin;
+    
+    const sec = decMin * 60;
+    let floorSec = Math.floor(sec);
+    const decSec = sec - floorSec;
+
+    let milSec = Math.floor(decSec * 100);
+
+    console.log(milSec);
+    return addZero(floorMin) + ":" + addZero(floorSec) + ":" + addZero(milSec)// milSec[0] + milSec[1];
+}
+
+
+
 function play()
 {
-    pause = false;
-    setInterval(() => {
-        if(!pause) {
+    tm = String(time);
+    isPaused = false;
+    interval = setInterval(() => {
+        if(!isPaused) {
+            const getTime = () => curTime();
             time++;
-            timer.innerHTML = time;
+            timer.innerHTML = "time: " + getTime();
         };
-    }, 1000);
+    }, 10);
 }
 
 function pause()
 {
-    pause = true;
+    isPaused = true;
+    clearInterval(interval);
 }
 
 function reset()
 {
-    pause = true;
+    clearInterval(interval);
+    isPaused = true;
     time = 0;
+    timer.innerHTML = "time: 0:00:00";
 }
